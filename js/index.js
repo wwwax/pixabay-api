a21eb8e6d6fd37ab4ffdfcba50fb34faa242273f
Max Ivanov <waxivanov@gmail.com>
2020-10-09 11:04:52 +0300

    add start fetch fn

--- js/index.js
+++ js/index.js
@@ -1,42 +1,57 @@
-"use strict";
+'use strict';
 
 // ===================================
 // VARIABLES
 // ===================================
 
 const refs = {
-  page: document.querySelector(".page"),
-  grid: document.querySelector(".grid"),
-  form: document.querySelector(".form"),
-  queryInput: document.querySelector(".input"),
-  showMoreButton: document.querySelector(".load-more")
+  page: document.querySelector('.page'),
+  grid: document.querySelector('.grid'),
+  form: document.querySelector('.form'),
+  queryInput: document.querySelector('.input'),
+  showMoreButton: document.querySelector('.load-more'),
 };
 
 const request = {
   page: 1,
-  query: "",
+  query: '',
 
   resetPage() {
-    refs.grid.innerHTML = "";
+    refs.grid.innerHTML = '';
     this.page = 1;
   },
+
   incrementPage() {
     this.page += 1;
-  }
+  },
 };
 
-const API_KEY = "7231268-668b9782a3d5ba8acee68f193";
+const API_KEY = '7231268-668b9782a3d5ba8acee68f193';
+
+start();
 
 // ===================================
 // EVENT LISTENERS
 // ===================================
 
-refs.form.addEventListener("submit", handleFormSubmit);
-refs.showMoreButton.addEventListener("click", handleShowMoreButtonClick);
+refs.form.addEventListener('submit', handleFormSubmit);
+refs.showMoreButton.addEventListener('click', handleShowMoreButtonClick);
 
 // ===================================
 // FUNCTIONS
 // ===================================
+
+function start() {
+  toggleLoader();
+
+  fetchImages({ query: 'nature', page: 1 })
+    .then((items) => {
+      toggleLoader();
+      const markup = getGridItems(items);
+      refs.grid.insertAdjacentHTML('beforeend', markup);
+    })
+    .catch((error) => console.log(error));
+}
 
 function handleFormSubmit(event) {
   event.preventDefault();
@@ -60,35 +75,35 @@ function handleFetch() {
   toggleLoader();
 
   fetchImages({ query: request.query, page: request.page })
-    .then(items => {
+    .then((items) => {
       toggleLoader();
       const markup = getGridItems(items);
-      refs.grid.insertAdjacentHTML("beforeend", markup);
+      refs.grid.insertAdjacentHTML('beforeend', markup);
     })
-    .catch(error => console.log(error));
+    .catch((error) => console.log(error));
 }
 
 function fetchImages({ query, page }) {
   const url = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&per_page=9&page=${page}`;
 
   return fetch(url)
-    .then(response => {
+    .then((response) => {
       if (response.ok) return response.json();
 
-      throw new Error("Error while fetching");
+      throw new Error('Error while fetching');
     })
-    .then(data => data.hits)
-    .catch(error => console.log(error));
+    .then((data) => data.hits)
+    .catch((error) => console.log(error));
 }
 
 function getGridItems(items) {
   return items.reduce(
     (acc, element) =>
       acc + `<div class="grid-item"><img src="${element.webformatURL}"></div>`,
-    ""
+    '',
   );
 }
 
 function toggleLoader() {
-  refs.page.classList.toggle("show-loader");
+  refs.page.classList.toggle('show-loader');
 }
